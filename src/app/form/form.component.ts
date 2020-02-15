744fac27d13ca5fb419c852b255acbdc2987c330
gaurecasm <57690334+gaurecasm@users.noreply.github.com>
2020-02-16 05:09:10 +0530

    Front end
    front end

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+  onAddPost(form: NgForm) {
+
+
+  }
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
